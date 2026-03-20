ddbc489a28d08f3bc21756e31c84fa0b5d29952d
Harry Chopra <22281143+harrychopra@users.noreply.github.com>
2026-03-20 09:18:17 +0000

    fix: update image validation

--- src/middleware/upload.middleware.js
+++ src/middleware/upload.middleware.js
@@ -7,7 +7,7 @@ const upload = multer({
   fileFilter: (_, file, cb) => {
     const allowed = ['image/jpeg', 'image/png'];
     if (!allowed.includes(file.mimetype)) {
-      return cb(new ApiError('Only JPEG and PNG images are allowed'));
+      return cb(new ApiError('Only JPEG and PNG images are allowed', 400));
     }
     cb(null, true);
   }
